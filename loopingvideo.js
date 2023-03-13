f2eea19fee47e56cc57edc2a8b8655448145b511
Chris Joseph <christopherzaworski@gmail.com>
2023-03-13 01:02:05 +0000

    matt got close

--- loopingvideo.js
+++ loopingvideo.js
@@ -1,6 +1,11 @@
 var vid = document.getElementById("bgvideo");
-vid.onended = loopFunction(){
-    alert("loopFunction called");		
-	vid.setAttribute("src", "resources/loopingVinyl.mp4")
+// vid.onended = loopFunction()
+
+
+function loopFunction() {
+    //alert("loopFunction called")		
+	vid.setAttribute('src', "resources/loopingVinyl.mp4")
 	vid.loop = true
+	vid.play();
+	
 };
