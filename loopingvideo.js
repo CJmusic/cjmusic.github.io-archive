d5e2d96e2eb4ed824534793db7e3967ad9e2e4d3
Chris Joseph <christopherzaworski@gmail.com>
2023-03-13 00:35:01 +0000

    removed superfluous code, making a custom solution

--- loopingvideo.js
+++ loopingvideo.js
@@ -1,18 +1,6 @@
-  var count = 1;
-  var vid = document.getElementById("bgvideo");
-  vid.addEventListener("ended", switchvideo, false);
-  function switchvideo(e) {
-
-    if (count > 1) {
-      vid.setAttribute('src', 'resources/loopingVinyl.mp');
-    } else {
-      vid.setAttribute('src', 'resources/IMG_0064.mp4');
-    }
-    count++;
-    vid.load();
-    try {
-      setTimeout(()=>vid.play(), 2000);
-    } catch (err){
-       console.log(err)
-    }
-  }
+var vid = document.getElementById("bgvideo");
+vid.onended = loopFunction(){
+    alert("loopFunction called");		
+	vid.setAttribute("src", "resources/loopingVinyl.mp4")
+	vid.loop = true
+};
