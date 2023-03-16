846d3022593db9aa6dc0f5a43e2c66e58b9a9204
Chris Joseph <christopherzaworski@gmail.com>
2023-03-16 16:39:57 +0000

    added hackercss theme, need to get Jekyll working

--- replit.nix
+++ replit.nix
@@ -1,5 +1,8 @@
 { pkgs }: {
   deps = [
+    pkgs.ruby_3_0
+    pkgs.rubyPackages_3_0.jekyll
+    pkgs.nodejs-16_x
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
