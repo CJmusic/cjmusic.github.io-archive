80da4da1c3df97e85f4dcf122d0ab62f5859f113
Chris Joseph <christopherzaworski@gmail.com>
2023-03-16 21:20:44 +0000

    Almost got it working.

--- replit.nix
+++ replit.nix
@@ -1,7 +1,7 @@
 { pkgs }: {
   deps = [
-    pkgs.ruby_3_0
-    pkgs.rubyPackages_3_0.jekyll
+    pkgs.ruby_2_7
+    pkgs.rubyPackages_2_7.jekyll
     pkgs.nodejs-16_x
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
